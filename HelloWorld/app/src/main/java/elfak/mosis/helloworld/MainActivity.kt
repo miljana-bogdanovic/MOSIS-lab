1518bd6789793bbf44578c91db28fe7135483a5a
miljana-bogdanovic <bogmiljana@gmail.com>
2022-04-27 23:30:59 +0200

    Ispravka

--- HelloWorld/app/src/main/java/elfak/mosis/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/elfak/mosis/helloworld/MainActivity.kt
@@ -28,17 +28,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onPause(){
-        super.onPause()
         Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
     }
 
     override fun onStop(){
-        super.onStop()
         Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+        super.onStop()
     }
 
     override fun onDestroy(){
-        super.onDestroy()
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
